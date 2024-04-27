c866e25fc86e3f5495d2d2e65f208726bb286810
Khushi <khushiagrawl0302@gmail.com>
2024-04-27 13:38:41 +0530

    initiated chrome browser added test to open the url

--- src/main/java/com/khushiagrawall/practice/Demo.java
+++ src/main/java/com/khushiagrawall/practice/Demo.java
@@ -1,4 +1,18 @@
 package com.khushiagrawall.practice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Demo {
+    public static void main(String[] args) {
+        // WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+//        try {
+//            Thread.sleep(5000); // Wait for 5 seconds
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        driver.close(); // Close the browser
+    }
 }
--- src/test/java/com/khushiagrawall/practice/DemoTest.java
+++ src/test/java/com/khushiagrawall/practice/DemoTest.java
@@ -1,4 +1,25 @@
 package com.khushiagrawall.practice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class DemoTest {
+    WebDriver driver=new ChromeDriver();
+    @BeforeMethod
+    public void setUp(){
+        driver.get("https://www.google.com");
+    }
+    @Test
+    public void DemoTest() throws InterruptedException {
+        Thread.sleep(5000);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
 }
